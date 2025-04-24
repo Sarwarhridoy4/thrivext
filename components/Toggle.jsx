a8e51ed8c4f7047ae234d3060fcb4b98c52a38d2
Takib-Ahmed <satakib300@gmail.com>
2025-04-24 11:52:21 +0600

    JavaScript Execution Context updated

--- components/Toggle.jsx
+++ components/Toggle.jsx
@@ -7,16 +7,16 @@ export default function Toggle({ title, children }) {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className=" cursor-pointer">
+    <div className="  ">
       <button
         onClick={() => setOpen(!open)}
         className="flex items-center gap-2 text-left font-medium transition-colors  w-full"
       >
     <BiSolidDownArrow className={`${open ? '':' -rotate-90'}  transition-all duration-300 ease-out cursor-pointer`}/>
-        <span>{title}</span>
+        <span title='Click to toggle' className=' cursor-pointer w-full'>{title}</span>
       </button>
       {open && (
-        <div className="mt-2 ml-6   pl-4 text-sm cursor-pointer    ">
+        <div className="mt-2 ml-6   pl-4 text-sm    ">
           {children}
         </div>
       )}
