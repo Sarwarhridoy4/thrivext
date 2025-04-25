5f1e10b97070356168857c83679e996e6425685a
Takib-Ahmed <satakib300@gmail.com>
2025-04-25 23:11:17 +0600

    Testing 2

--- app/JavaScript_Essentials_for_React/_meta.js
+++ app/JavaScript_Essentials_for_React/_meta.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-    Javascript_Key_Features_and_Concepts: "JavaScript Key Features & Concepts",
+    Javascript_Key_Features_and_Concepts: "JavaScript Key Features and Concepts",
     Js_Execution_Process: "JS Execution Process",
     Javascript_Scope: "JavaScript Scope",
   };
