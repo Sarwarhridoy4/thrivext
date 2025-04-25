0269e122ce425a01516592940a36cf4c614cd5a5
Takib Ahmed <satakib300@gmail.com>
2025-04-25 23:38:05 +0600

    Add files via upload

---
+++ app/JavaScript_Essentials_for_React/_meta.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  Javascript_Key_Features_and_Concepts: "JavaScript Key Features and Concepts",
+  Javascript_Scope: "JavaScript Scope",
+  Js_Execution_Process: "JS Execution Process",
+};
+
