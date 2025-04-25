280a9dfa77dfdec7425fe0412aedf8dd8fc1c5d5
Takib-Ahmed <satakib300@gmail.com>
2025-04-25 23:15:37 +0600

    meta.js updated

--- app/JavaScript_Essentials_for_React/_meta.js
+++ app/JavaScript_Essentials_for_React/_meta.js
@@ -1,7 +1,12 @@
+
+
+
 /* eslint-disable import/no-anonymous-default-export */
+
 export default {
-    Javascript_Key_Features_and_Concepts: "JavaScript Key Features and Concepts",
-    Js_Execution_Process: "JS Execution Process",
-    Javascript_Scope: "JavaScript Scope",
-  };
+  Javascript_Key_Features_and_Concepts: "JavaScript Key Features and Concepts",
+  Javascript_Scope: "JavaScript Scope",
+  Js_Execution_Process: "JS Execution Process",
+};
+
   
