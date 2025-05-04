ccad4cc417129457aec6d0330a38c2f33d9992ae
Takib-Ahmed <satakib300@gmail.com>
2025-05-04 11:55:38 +0600

    why do we need ts config set up added

---
+++ app/Typescript/Module_1/ts_file_setup/_meta.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    Why_setup: "Why do we need tsconfig setup?",
+    ts_confiq_setup: "⚙️ tsconfig.json কনফিগারেশন",
+ 
+
+  };
+  
