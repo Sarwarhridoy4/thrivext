24431536c829818d1a51b174c3b644315ffc7cdf
Rafsan12 <rafsan.web.2022@gmail.com>
2025-05-06 07:35:19 +0600

    Update:Type_Alias_and_Union_Types part

--- app/Typescript/Module_1/Basic_TypeScript_Data_Types/_meta.js
+++ app/Typescript/Module_1/Basic_TypeScript_Data_Types/_meta.js
@@ -4,7 +4,7 @@ export default {
   Array_Tuple: "Array and Tuple",
   Object_Function: "Object and Function",
   Spread_Rest_Destructuring: "Spread, Rest and Destructuring",
-  Type_Alias: "Type Alias and Union Types",
+  Type_Alias_and_Union_Types: "Type Alias and Union Types",
   Intersection_Ternary_Operator: "Intersection and Ternary Operator",
   Nullish_Optional_Nullable_Unknown_Never:
     "Nullish, Optional, Nullable, Unknown and Never",
