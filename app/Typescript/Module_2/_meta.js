cad66cd914ba130cf2dc09fe411667011a8e2add
Hamim-1 <hmd874264@gmail.com>
2025-05-12 23:24:28 +0600

    module two added

---
+++ app/Typescript/Module_2/_meta.js
@@ -0,0 +1,9 @@
+export default {
+  Type_Assertion_type_narrowing: "Type Assertion and type narrowing",
+  Interface_type_vs_interface: "Interface, type vs interface",
+  Introduction_to_Generics: "Introduction to generics",
+  Generic_with_Interface: "Generic with Interface",
+  Function_with_Generics: "Function with generics",
+  Constraints_in_TypeScript: "Constraints in typescript",
+  Constraint_using_keyof: "Constraint using key of",
+};
