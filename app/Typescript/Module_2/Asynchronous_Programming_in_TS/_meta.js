fc088ab9687aa71374c7ecfd0a4ebbe3b6fe4111
Takib-Ahmed <satakib300@gmail.com>
2025-05-20 23:46:55 +0600

    module-2: Advanced types of typeScript docs added

---
+++ app/Typescript/Module_2/Asynchronous_Programming_in_TS/_meta.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+
+   Basics_of_Asynchronous_Programming: "Asynchronous Basics",
+   Asynchronous_TypeScript: "Asynchronous TypeScript",
+
+
+};
+
--- app/Typescript/Module_2/_meta.js
+++ app/Typescript/Module_2/_meta.js
@@ -1,9 +1,12 @@
 export default {
   Type_Assertion_type_narrowing: "Type Assertion and type narrowing",
   Interface_type_vs_interface: "Interface, type vs interface",
-  Introduction_to_Generics: "Introduction to generics",
+  Introduction_to_Generics: "Introduction to Generics",
+  Generics_with_object_array_and_tuple:"Generics with object array & tuple",
   Generic_with_Interface: "Generic with Interface",
-  Function_with_Generics: "Function with generics",
+  Function_with_Generics: "Function with Generics",
   Constraints_in_TypeScript: "Constraints in typescript",
   Constraint_using_keyof: "Constraint using key of",
+  Asynchronous_Programming_in_TS:'Asynchronous Programming in TS',
+  Type_Checking_and_Conditionals:'Type Checking & Conditionals'
 };
