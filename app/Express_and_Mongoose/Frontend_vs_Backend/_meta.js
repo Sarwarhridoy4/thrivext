3afcc7d40c9bdbb7e3f6df2a04e810b20f517e7f
Takib-Ahmed <satakib300@gmail.com>
2025-06-12 21:38:18 +0600

    module-12.2 : Frontend vs Backend

---
+++ app/Express_and_Mongoose/Frontend_vs_Backend/_meta.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  Definitions_of_Frontend_and_Backend: "Definitions of Frontend & Backend", //1
+  Static_and_Dynamic_website:'Static & Dynamic website',
+   SSR_vs_CSR: "SSR vs CSR",//2
+
+};
+
