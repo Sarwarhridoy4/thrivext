2659f3eb06a0d59046e4a40a295608f3cf8f1954
Takib-Ahmed <satakib300@gmail.com>
2025-06-16 23:44:38 +0600

    Module 12 : IIFEE, commonjs, ES module

---
+++ app/Express_and_Mongoose/Modular_Node.js/Commonjs_Module/_meta.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+     Commonjs_and_Local_Module: "CommonJS & Local Module", //2
+  Deep_Dive_Alias_and_Indexjs: "Deep Dive Alias & Index.js", //1
+ 
+
+};
---
+++ app/Express_and_Mongoose/Modular_Node.js/_meta.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  System_of_Nodejs_Module: "System of Node.js Module", //1
+  Commonjs_Module: "CommonJS Module", //2,
+  IIFE:'IIFE',
+    ES_Module: "ES Module", //3,
+
+
+
+  
+};
